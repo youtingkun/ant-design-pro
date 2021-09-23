c1a92afec975da3556e910debbadd19c8c0db43e
youtingkun <youtingkun@techsailing.com>
2021-09-23 20:31:25 +0800

    feat: 测试api

--- config/proxy.ts
+++ config/proxy.ts
@@ -9,7 +9,7 @@
 export default {
   dev: {
     '/api/': {
-      target: 'https://preview.pro.ant.design',
+      target: 'http://yapi.youtingkun.com/mock/382',
       changeOrigin: true,
       pathRewrite: { '^': '' },
     },
