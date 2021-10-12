b2c031ec6208232a294990436ba5cf1f151c2bec
youtingkun <youtingkun@techsailing.com>
2021-10-12 14:08:58 +0800

    feat: test

--- src/pages/temp/index.ts
+++ src/pages/temp/index.ts
@@ -1,19 +1,4 @@
-function format(params: string) {
-  console.log(params); // www.baidu.com，当需要追加自定义参数时可以这么写，也称作装饰器工厂
-  return function (target: any, propertyKey: string) {
-    console.log(target); // {constructor: ƒ}
-    console.log(propertyKey); // greeting
-  };
-}
-
-class Greeter {
-  @format('www.baidu.com')
-  greeting: string;
-
-  constructor(message: string) {
-    this.greeting = message;
-  }
-}
-const a = new Greeter('123');
-
-console.log(a);
+let num: number | undefined; // 数值类型 或者 undefined
+console.log(num); // 正确
+num = 123;
+console.log(num); // 正确
--- src/pages/temp/index.tsx
+++ src/pages/temp/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.ts';
 
 const Temp: React.FC = () => {
   return <div>temp</div>;
